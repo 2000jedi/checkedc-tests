54816eb13df1b23d0be1a8d4b0059a1c68a2fd3a
Yudi Yang <yyang116@u.rochester.edu>
2020-02-27 15:53:06 -0500

    updates to dynamic cast test

--- test_dynamic.c
+++ test_dynamic.c
@@ -4,17 +4,20 @@
 //#include <stdlib_checked.h>
 #include <stdio.h>
 #include <string.h>
-/* Turn off the checked scope for main fucntion */
-// #pragma CHECKED_SCOPE OFF
+_Unchecked void pc(char p) {
+  putchar(p);
+}
 
-#undef memset
-void *memset(void * dest : byte_count(n),
-             int c,
-             size_t n) : bounds(dest, (_Array_ptr<char>)dest + n);
-
-int foo(int * checked_array : itype(_Array_ptr<int>) count(len), size_t len){
-	puts("Hello!");
-	return 1;
+_Checked int foo(int * checked_array : itype(_Array_ptr<int>) count(len), size_t len){
+	pc('F');
+  pc('o');
+  pc('o');
+  pc(':');
+  pc(' ');
+  for (int i = 0; i < len; ++i)
+    pc(checked_array[i] + '0');
+  pc('\n');
+  return 1;
 }
 
 /*
@@ -22,15 +25,13 @@ int foo(int * checked_array : itype(_Array_ptr<int>) count(len), size_t len){
   Have not figured out why does this bug happen
  */
 int main(int argc, char** argv) {
-  int a _Checked[5];
+  int a _Checked[5] = {5, 4, 3, 2, 1};
   size_t l = 4;
-  
-  memset(a, 5, sizeof(int) * 5);
   
   /* Directly using bounds cast inside function does not work */
   //#if 0
-  foo(_Dynamic_bounds_cast<_Array_ptr<int>>(a, count(l)), l);
-  foo(_Assume_bounds_cast<_Array_ptr<int>>(a, count(l)), l);
+  //foo(_Dynamic_bounds_cast<_Array_ptr<int>>(a, count(l)), l);
+  //foo(_Assume_bounds_cast<_Array_ptr<int>>(a, count(l)), l);
   //#endif
   
   /* Assigning to a new variable does not work */
